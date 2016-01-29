7a658fdcdc6ee9b6ef9845eed84332bf9c40b91e
Fred <fredpaw@gmail.com>
2016-01-29 11:22:22 +1100

    framework finished

--- wp-content/themes/chuguo/footer.php
+++ wp-content/themes/chuguo/footer.php
@@ -102,20 +102,20 @@
 				<footer class='container_wrap socket_color' id='socket' <?php avia_markup_helper(array('context' => 'footer')); ?>>
 					<div id='socket-company-detail-wrapper'>
 						<div class="av-layout-grid-container entry-content-wrapper av-flex-cells container" id='socket-company-detail'>
-							<div class="flex_cell no_margin av_two_third  avia-builder-el-2  el_before_av_cell_one_third  avia-builder-el-first no-padding">
+							<div class="flex_cell no_margin av_one_half  avia-builder-el-35  el_before_av_cell_one_half  avia-builder-el-first no-padding">
 								<div class="flex_cell_inner company-detail">
 									<p class="font-bold" style="line-height:23px;"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/cg16_home_footer_icon01.png" /> 成功出国留学网&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/cg16_home_footer_icon02.png" /> 法兰克澳洲留学移民事务所 中国代表处</p>
 									<p style="font-size:32px;line-height:32px;">提供一站式澳大利亚留学移民专业服务</p>
-									<p style="margin-left:30px;margin-top:40px; margin-bottom:30px;"><span style="font-size:26px; color:#fff; display:inline-box; -webkit-border-radius: 5; -moz-border-radius: 5; border-radius: 5px;
+									<p style="margin-top:40px; margin-bottom:30px;"><span style="font-size:26px; color:#fff; display:inline-box; -webkit-border-radius: 5; -moz-border-radius: 5; border-radius: 5px;
   background: #3498db; padding: 15px 30px 15px 30px;";>成立于1999年</span></p>
 									<p><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/cg16_home_footer_site.png" /> 北京市朝阳区东大桥路8号soho尚都南塔2805室</p>
 									<p><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/cg16_home_footer_phone.png" /> 010-59003028</p>
 									<p><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/cg16_home_footer_email.png" /> services@cg6xue.com</p>
 								</div>
 							</div>
-							<div class="flex_cell no_margin av_one_third  avia-builder-el-3  el_after_av_cell_two_third  avia-builder-el-last no-padding">
+							<div class="flex_cell no_margin av_one_half  avia-builder-el-36  el_after_av_cell_one_half  avia-builder-el-last no-padding">
 								<div class="flex_cell_inner qr-code">
-									<p class="font-center"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/cg16_weixin.png" /></p>
+									<p><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/cg16_home_footer1.png" /></p>
 								</div>
 							</div>
 						</div>
--- wp-content/themes/chuguo/functions.php
+++ wp-content/themes/chuguo/functions.php
@@ -6,7 +6,9 @@ function theme_enqueue_styles() {
 
 }
 
-//Footer Column Widgets Register
+/**
+*	Footer Column Widgets Register
+*/
 class footerColumn1Widget extends WP_Widget {
 	function __construct() {
 		parent::__construct(false, 'Footer Column 1 Widget');
@@ -100,15 +102,78 @@ class footerColumn5Widget extends WP_Widget {
 	}
 }
 
-function footercolumnwidgets_register_widgets() {
+/**
+*	Sidebar Widgets
+*/
+
+// Sidebar links
+class sidebarLinksWidget extends WP_Widget {
+	function __construct() {
+		parent::__construct(false, 'Sidebar Links');
+	}
+	
+	function widget() {
+		?>
+		<div id="sidebar-links">
+		<p>
+			<a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/cg16_home_con01_pic01.png"/> <span>留学移民服务</span></a>
+			<a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/cg16_home_con01_pic02.png"/> <span>微信讲座</span></a>
+			<a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/cg16_home_con01_pic03.png"/> <span>在线咨询</span></a>
+		</p>
+		</div>
+		<?php
+	}
+}
+
+// Sidebar News
+class sidebarNewsWidget extends WP_Widget {
+	function __construct() {
+		parent::__construct(false, 'Sidebar News');
+	}
+	
+	function widget() {
+		echo '<div id="sidebar-news">';
+		echo '<h2>经验与资讯</h3>';
+		$args = array(
+			'cat' => 3
+		);
+		
+		$the_query = new WP_Query( $args );
+
+		if ( $the_query->have_posts() ) :
+		$post_count = 0;
+		while ( $the_query->have_posts() && $post_count < 2 ) : $the_query->the_post();
+			echo '<div class="news-block">';
+			echo '<div class="news-preview">';
+			echo '<h3><a href="'.get_the_permalink().'">'.get_the_title().'</a></h3>';
+			echo '<p>'.get_the_excerpt().'</p>';
+			echo '</div>';
+			echo '<div class="news-preview-image">'.get_the_post_thumbnail().'</div>';
+			echo '</div>';
+			$post_count++;
+		endwhile;
+		endif;
+
+		wp_reset_postdata();
+		echo '</div>';
+	}
+}
+
+function footer_sidebar_register_widgets() {
 	register_widget('FooterColumn1Widget');
 	register_widget('FooterColumn2Widget');
 	register_widget('FooterColumn3Widget');
 	register_widget('FooterColumn4Widget');
 	register_widget('FooterColumn5Widget');
+	register_widget('sidebarLinksWidget');
+	register_widget('sidebarNewsWidget');
 }
 
-add_action('widgets_init','footercolumnwidgets_register_widgets');
+add_action('widgets_init','footer_sidebar_register_widgets');
+
+
+
+// Sidebar news loop
 
 /**
 *	Handling Shortcode
@@ -120,7 +185,7 @@ function mainsearch_func () {
     $form ='<form action="' . home_url( '/' ) . '" id="mainsearch" method="get" class="">
 			<div>
 				<input type="submit" value=" 搜索" id="searchsubmit" class="button avia-font-entypo-fontello">
-				<input type="text" id="s" name="s" value="" placeholder="Search">
+				<input type="text" id="s" name="s" value="" placeholder="搜索">
 			</div>
 			</form>';
 	return $form;
@@ -128,9 +193,158 @@ function mainsearch_func () {
 add_shortcode('mainsearch', 'mainsearch_func');
 
 // Breadcrumbs Shortcode
+//[pagebreadcrumb]
 function pagebreadcrumb_func () {
-	return ;
+	global $avia_config;
+
+	$id = avia_get_the_id();
+	
+	$defaults 	 = array(
+
+		'title' 		=> get_the_title($id),
+		'subtitle' 		=> "", //avia_post_meta($id, 'subtitle'),
+		'link'			=> get_permalink($id),
+		'html'			=> "<div class='{class} title_container page_breadcrumb'><div class='container'><{heading} class='main-title entry-title'>{title}</{heading}>{additions}</div></div>",
+		'class'			=> 'stretch_full container_wrap alternate_color '.avia_is_dark_bg('alternate_color', true),
+		'breadcrumb'	=> true,
+		'additions'		=> "",
+		'heading'		=> 'h1' //headings are set based on this article: http://yoast.com/blog-headings-structure/
+	);
+
+	if ( is_tax() || is_category() || is_tag() )
+	{
+		global $wp_query;
+
+		$term = $wp_query->get_queried_object();
+		$defaults['link'] = get_term_link( $term );
+	}
+	else if(is_archive())
+	{
+		$defaults['link'] = "";
+	}
+	
+	
+	// Parse incomming $args into an array and merge it with $defaults
+	$args = wp_parse_args( $args, $defaults );
+	$args = apply_filters('avf_title_args', $args, $id);
+
+	//disable breadcrumb if requested
+	//$args['breadcrumb'] = false;
+	
+	//disable title if requested
+	$args['title'] = '';
+
+
+	// OPTIONAL: Declare each item in $args as its own variable i.e. $type, $before.
+	extract( $args, EXTR_SKIP );
+
+	if(empty($title)) $class .= " empty_title ";
+	$markup = avia_markup_helper(array('context' => 'avia_title','echo'=>false));
+	if(!empty($link) && !empty($title)) $title = "<a href='".$link."' rel='bookmark' title='".__('Permanent Link:','avia_framework')." ".esc_attr( $title )."' $markup>".$title."</a>";
+	if(!empty($subtitle)) $additions .= "<div class='title_meta meta-color'>".wpautop($subtitle)."</div>";
+	if($breadcrumb) $additions .= avia_breadcrumbs(array('separator' => '/', 'richsnippet' => true));
+
+
+	$html = str_replace('{class}', $class, $html);
+	$html = str_replace('{title}', $title, $html);
+	$html = str_replace('{additions}', $additions, $html);
+	$html = str_replace('{heading}', $heading, $html);
+
+
+
+	if(!empty($avia_config['slide_output']) && !avia_is_dynamic_template($id) && !avia_is_overview())
+	{
+		$avia_config['small_title'] = $title;
+	}
+	else
+	{
+		return $html;
+	}
 }
 add_shortcode('pagebreadcrumb', 'pagebreadcrumb_func');
+
+// Post Content Shortcode
+// [postbrief id='']
+function postbrief_func($attr) {
+	$atts = shortcode_atts(array('id'=>'post id'),$attr);
+	$post_title = get_post($atts['id'])->post_title;
+	$post_content = get_post($atts['id'])->post_excerpt;
+	$post_link = get_permalink($atts['id']);
+	return '<div class="postbrief">
+				<h3>'.$post_title.'</h3>
+				<p>'.$post_content.'</p>
+				<p><a href="'.$post_link.'">阅读更多<span class="more-link-arrow">  →</span></a></p>
+			</div>';
+}
+add_shortcode ('postbrief','postbrief_func');
+
+//	Page Sider Title Shortcode
+//	[pageslidertitle]
+function pageslidertitle_func() {
+	$html = '<div style="padding-bottom:40px;"></div>';
+	$title = '';
+	if( is_page() ) {
+		$link_array = explode('/', str_replace(get_bloginfo('url').'/', '' ,get_the_permalink()));
+		$title = $link_array[0];
+	} else if( is_single() ) {
+		$cate = get_the_cat();
+		$cslug = $cate->slug;
+		if( $cslug = 'experience_info')
+			$title = 'experience-and-consulting';
+	}
+	switch ($title) {
+		case 'study-immigration':
+			$chinese_title = '留学+移民';
+			$english_title = 'STUDY & IMMIGRATION';
+			break;
+		case 'australian-university':
+			$chinese_title = '读大学';
+			$english_title = 'UNIVERSITY';
+			break;
+		case 'australian-high-school':
+			$chinese_title = '读中学';
+			$english_title = 'SCHOOLS';
+			break;
+		case 'art-students':
+			$chinese_title = '艺术生';
+			$english_title = 'ART & DESIGN';
+			break;
+		case 'visit-australian':
+			$chinese_title = '探访澳洲';
+			$english_title = 'VISITING AUSTRALIA';
+			break;
+		case 'experience-and-consulting':
+			$chinese_title = '经验与资讯';
+			$english_title = 'EXPERIENCE STORY NEWS';
+			break;
+		case 'our-service':
+			$chinese_title = '我们的服务';
+			$english_title = 'OUR SERVICE';
+			break;
+		default:
+			$chinese_title = '成立于1999年';
+			$english_title = '不成功不收费';
+	}
+	$html .= '<div id="chinese_title"><p>'.$chinese_title.'</p></div>';
+	$html .= '<div id="english_title"><p>'.$english_title.'</p></div>';
+	$html .= '<img style="margin-top:30px;" src="'.get_stylesheet_directory_uri().'/images/cg16_secondary_bannerleftfont.png"/>';
+	return $html;
+}
+add_shortcode ('pageslidertitle','pageslidertitle_func');
+
+/**
+*	Post Excerpt Changes
+*/
+// Change excerpt symbol
+function new_excerpt_more( $more ) {
+	return '...';
+}
+add_filter('excerpt_more', 'new_excerpt_more');
+
+// Change excerpt length
+function custom_excerpt_length( $length ) {
+	return 25;
+}
+add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 ?>
 
